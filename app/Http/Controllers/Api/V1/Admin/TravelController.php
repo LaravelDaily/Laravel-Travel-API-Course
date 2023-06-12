c8b922d868c879109f29a587e64259c277f2a166
PovilasKorop <povilask007@gmail.com>
2023-06-12 11:32:16 +0300

    Create travel endpoint - unprotected yet

---
+++ app/Http/Controllers/Api/V1/Admin/TravelController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\TravelRequest;
+use App\Http\Resources\TravelResource;
+use App\Models\Travel;
+
+class TravelController extends Controller
+{
+    public function store(TravelRequest $request)
+    {
+        $travel = Travel::create($request->validated());
+
+        return new TravelResource($travel);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\V1\TravelController;
 use App\Http\Controllers\Api\V1\TourController;
+use App\Http\Controllers\Api\V1\Admin;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,3 +24,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('travels', [TravelController::class, 'index']);
 Route::get('travels/{travel:slug}/tours', [TourController::class, 'index']);
+
+Route::prefix('admin')->group(function () {
+    Route::post('travels', [Admin\TravelController::class, 'store']);
+});
