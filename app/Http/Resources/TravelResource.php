0f40f78dda79d16e3034efb116fabee9a521ff88
PovilasKorop <povilask007@gmail.com>
2023-06-14 09:35:01 +0300

    Larastan

--- app/Http/Resources/TravelResource.php
+++ app/Http/Resources/TravelResource.php
@@ -5,6 +5,10 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/** 
+* @mixin \App\Models\Travel 
+* @property int $number_of_nights
+*/
 class TravelResource extends JsonResource
 {
     /**
